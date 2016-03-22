34a3d497fff9a59569d1b7902113eee7e13e9cef
Ailtonjr <ailtoncardoso0@gmail.com>
2016-03-22 14:04:36 -0300

    Adicionado Classe Usuario

---
+++ Waypoints-Server/src/java/modelo/Usuario.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ *
+ * @author marcelosiedler
+ */
+public class Usuario {
+    
+    private String usuario;
+    private String senha;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String login) {
+        this.usuario = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    } 
+    
+}
