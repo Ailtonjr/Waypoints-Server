479758484fe1c59e42dced8285d408051be8c040
Ailtonjr <ailtoncardoso0@gmail.com>
2016-03-22 14:04:24 -0300

    Adicionado Classe UsuarioDAO

---
+++ Waypoints-Server/src/java/dao/UsuarioDAO.java
@@ -0,0 +1,159 @@
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Usuario;
+
+/**
+ *
+ * @author Ailton Jr
+ */
+public class UsuarioDAO {
+    
+    public UsuarioDAO()
+    {
+    
+    }
+    
+    public boolean inserir(Usuario usuario)
+    {
+        String sql = "INSERT INTO usuario(usuario,senha) VALUES(?,?)";
+        Boolean retorno = false;
+        PreparedStatement pst = Conexao.getPreparedStatement(sql);
+        try {
+            pst.setString(1, usuario.getUsuario());
+            pst.setString(2, usuario.getSenha());
+            
+            if(pst.executeUpdate()>0)
+            {
+                retorno = true;
+            }
+                
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            retorno = false;
+        }
+        
+        return retorno;
+    
+    }
+    public boolean atualizar(Usuario usuario)
+    {
+        String sql = "UPDATE usuario set senha=? where usuario=?";
+        Boolean retorno = false;
+        PreparedStatement pst = Conexao.getPreparedStatement(sql);
+        try {
+          
+            pst.setString(1, usuario.getSenha());
+            pst.setString(4, usuario.getUsuario());
+            if(pst.executeUpdate()>0)
+            {
+                retorno = true;
+            }
+                
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            retorno = false;
+        }
+        
+        return retorno;
+    
+    }
+    public boolean excluir(Usuario usuario)
+    {
+        String sql = "DELETE FROM usuario where usuario=?";
+        Boolean retorno = false;
+        PreparedStatement pst = Conexao.getPreparedStatement(sql);
+        try {
+          
+           
+            pst.setString(1, usuario.getUsuario());
+            if(pst.executeUpdate()>0)
+            {
+                retorno = true;
+            }
+                
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            retorno = false;
+        }
+        
+        return retorno;
+    
+    }
+    
+    public List<Usuario> listar()
+    {
+         String sql = "SELECT * FROM usuario";
+        List<Usuario> retorno = new ArrayList<Usuario>();
+        
+        PreparedStatement pst = Conexao.getPreparedStatement(sql);
+        try {
+           
+            
+            ResultSet res = pst.executeQuery();
+            while(res.next())
+            {
+                Usuario item = new Usuario();
+                item.setUsuario(res.getString("usuario"));
+                item.setSenha(res.getString("senha"));
+                
+                retorno.add(item);
+            }
+               
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+        
+        return retorno;
+    
+    
+    }
+    public Usuario buscar(Usuario usuario)
+    {
+         String sql = "SELECT * FROM usuario where usuario=?";
+        Usuario retorno = null;
+        
+        PreparedStatement pst = Conexao.getPreparedStatement(sql);
+        try {
+           
+            pst.setString(1, usuario.getUsuario());
+            ResultSet res = pst.executeQuery();
+            
+            if(res.next())
+            {
+                retorno = new Usuario();
+                retorno.setUsuario(res.getString("usuario"));
+                retorno.setSenha(res.getString("senha"));
+                
+                
+            }
+               
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+        
+        return retorno;
+    
+    
+    }
+
+
+}
