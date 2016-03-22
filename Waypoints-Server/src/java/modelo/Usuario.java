801afe26c0d58ad25286c155432574148013f1ed
Ailtonjr <ailtoncardoso0@gmail.com>
2016-03-22 14:04:36 -0300

    Adicionado Classe Usuario

---
+++ Waypoints-Server/src/java/modelo/Usuario.java
@@ -0,0 +1,28 @@
+package modelo;
+
+/**
+ *
+ * @author Ailton Jr
+ */
+public class Usuario {
+    
+    private String usuario;
+    private String senha;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String login) {
+        this.usuario = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    } 
+    
+}
